ef80401612873a6ee88775b166db7bd82bf741d9
Burak Şen <burak.sen@tum.de>
2024-07-11 08:44:14 +0200

    MovementSystem implementation

---
+++ src/Game/Systems/MovementSystem/MovementSystem.cpp
@@ -0,0 +1,34 @@
+#include "MovementSystem.h"
+
+void MovementSystem::update(float deltaTime)
+{
+    this->m_registry.view<TransformComponent, EntityStateComponent>().each(
+        [deltaTime](auto entity, TransformComponent &transform, EntityStateComponent &state)
+        {
+            Vector2 direction = {0, 0};
+            if (IsKeyDown(KEY_W))
+            {
+                direction.y = -1;
+                state.direction = Direction::Up;
+            }
+            if (IsKeyDown(KEY_S))
+            {
+                direction.y = 1;
+                state.direction = Direction::Down;
+            }
+            if (IsKeyDown(KEY_A))
+            {
+                direction.x = -1;
+                state.direction = Direction::Left;
+            }
+            if (IsKeyDown(KEY_D))
+            {
+                direction.x = 1;
+                state.direction = Direction::Right;
+            }
+            direction = Vector2Normalize(direction);
+            float speed = 100.0f;
+            transform.position.x += direction.x * speed * deltaTime;
+            transform.position.y += direction.y * speed * deltaTime;
+        });
+}
